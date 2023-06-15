abf26d76bc24ef7414a1d6b67c1f9b48ba1a800f
Komron <komronabdujabbor@gmail.com>
2023-06-15 17:47:43 +0500

    main funksiya yaratildi

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
